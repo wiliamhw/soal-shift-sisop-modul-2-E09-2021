900f35a2755857093ac31e1950e0ec4a1feaa41b
William Handi Wijaya <52129348+wiliamhw@users.noreply.github.com>
2021-04-15 12:28:18 +0700

    Finish Soal2 (#1)
    * Finish soal2a
    
    * Finish soal2b
    
    * Fix folder cleaning on soal2b
    
    * Rename dir variable name
    
    * Improve modularity
    
    * Finish soal2c
    
    * Finish soal2d
    
    * Finish soal2e

--- README.md
+++ README.md
@@ -12,12 +12,99 @@
 # Soal 2
 ## Info
 * Dikerjakan oleh 05111940000087 - William Handi Wijaya.
+* Tidak boleh menggunakan: `system()`, *shell script*, `mkdir()`, dan `rename`.
+* Menggunakan `fork` dan `exec`.
   
+
 ## Subsoal a
 ### Penjelasan Soal
+Di soal ini kami disuruh:
+1. Extract pets.zip ke dalam folder `/home/[user]/modul2/petshop`.
+2. Hapus folder-folder yang ada dalam pets.zip.
 
 ### Penyelesaian Soal
-#### Perintah x
+#### Perintah 1
+1. Ubah current directory menjadi `/home/[user]/modul2/petshop`.
+   * Perintah: `chdir("/home/[user]/modul2/petshop")`.
+2. Download folder pets.zip jika belum ada.
+   * Perintah: `wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1g5rehatLEkqvuuK_eooHJXB57EfdnxVD -O pets.zip`
+3. Extract pets.zip dengan perintah `unzip pets.zip`.
+
+#### Perintah 2
+1. Jelajahi folder pets dengan code `listFilesRecursively.c` dari modul 2.
+2. Hapus setiap folder yang ditemukan di dalam pets.
+   1. Cek tiap path dengan `stat()` dan `S_ISREG`.
+   2. Jika path tersebut adalah folder, hapus folder tersebut dengan perintah `rm -r <path>`.
+
+
+## Subsoal b
+### Penjelasan Soal
+Di soal ini kami disuruh:
+1. Dapatkan jenis peliharaan dari nama file.
+2. Masukan file ke dalam folder sesuai dengan jenis peliharaan.
+   * Contoh: jenis peliharaan kucing akan disimpan dalam `/petshop/cat`.
+
+## Penyelesaian Soal
+Semua solusi di bawah ini dijalankan dalam fungsi yang menyerupai `listFilesRecursively.c`, yaitu fungsi `categorize`.
+
+### Perintah 1
+1. Pastikan file bukan berupa folder.
+2. Dapatkan nama file.
+3. Dapatkan jenis peliharaan dari nama file dengan perintah `strtok(<namafile>, ';')`.
+
+### Perintah 2
+1. Cek apakah folder jenis peliharaan sudah terbuat.
+2. Jika belum:
+   1. Buat folder jenis peliharaan dengan perintah `cp -r /home/<user>/log <jenis peliharaan>`.
+   2. Bersihkan folder `<jenis peliharaan>`.
+3. Pindahkan file ke folder jenis peliharaan dengan perintah `mv <nama file> <jenis peliharaan>`.
+
+
+## Subsoal c
+### Penjelasan Soal
+Di soal ini kami disuruh:
+* Rename nama file sesuai dengan nama peliharaannya.  
+  Contoh: `petshop/cat/joni.jpg`.
+
+### Penyelesaian Soal
+Solusi di bawah ini dilakukan di dalam fungsi `categorize`, sebelum file dipindahkan.
+1. Dapatkan nama hewan.
+2. Copy file menjadi file baru bernama `<nama peliharaan>`.
+3. Pindahkan file `<nama peliharaan>` ke folder yang tepat.
+4. Hapus file lama.
+
+
+## Subsoal d
+### Penjelasan Soal
+Di soal ini kami disuruh:
+1. Dapatkan jenis dan nama peliharaan kedua.
+2. Pindahkan file tersebut ke folder yang tepat untuk kedua kalinya.
+
+### Penyelesaian Soal
+Solusi ini akan memodifikasi solusi pada subsoal c.
+* Jika terdapat peliharaan kedua:
+   1. Lakukan solusi no.1-3 subsoal c untuk peliharaan kedua.
+   2. Setelah kedua file terpindah, hapus file lama.
+
+
+## Subsoal e
+### Penjelasan Soal
+Di soal ini kami disuruh:
+1. Membuat file `keterangan.txt` di setiap folder jenis peliharaan.
+2. Isi file `keterangan.txt` dengan nama dan umur semua peliharaan di folder yang sama.  
+   Format:
+   > nama : joni  
+   > umur : 3 tahun
+   >
+   > nama : miko  
+   > umur: 2 tahun
+   >
+
+### Penyelesaian Soal
+Solusi ini di antara solusi no.1 dan no.2 pada subsoal d.
+1. Dapatkan umur.
+2. Jika file `keterangan.txt` tidak ada, buat file tersebut dengan perintah `touch <nama folder>/keterangan.txt`.
+3. Isi `keterangan.txt` sesuai dengan format.
 <br><br>
 
 # Soal 3
---
+++ soal2/soal2.c
@@ -0,0 +1,216 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <wait.h>
+#include <dirent.h>
+#include <ctype.h>
+
+DIR *getDir(char *basePath);
+FILE *getLog(char *dirPath);
+char *getAge(char *input);
+bool isRegularFile(const char *path);
+void downloadExtract();
+void deleteFolder(char *basePath, DIR *dir, bool deleteFile);
+void categorize(DIR *dir);
+void command(const char *command, char *path); // download, extract, delete, make_dir, move, copy
+
+const char *user = "frain8";
+
+int main()
+{
+    // Make sure that folder exist
+    char petshop[50];
+    sprintf(petshop, "/home/%s/modul2", user);
+    DIR *tmp = getDir(petshop);
+    closedir(tmp);
+
+    strcat(petshop, "/petshop");
+    DIR *dir = getDir(petshop);
+
+    // Change curr dir
+    if ((chdir(petshop)) < 0) {
+        exit(EXIT_FAILURE);
+    }
+    downloadExtract(); // Download and extract pets.zip
+    
+    // Traverse dir
+    deleteFolder(".", dir, false); // Clear initial folder
+    categorize(dir);
+
+    closedir(dir);
+    return 0;
+}
+
+void downloadExtract()
+{
+    char *filename = "../pets.zip";
+    if (access(filename, F_OK) != 0) { // If pets.zip not exist
+        command("download", filename);
+    }
+    command("extract", filename);
+}
+
+void deleteFolder(char *basePath, DIR *dir, bool deleteFile)
+{
+    char path[1000];
+    struct dirent *dp;
+    while ((dp = readdir(dir)) != NULL) {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
+            // Construct new path from the base path
+            sprintf(path, "%s/%s", basePath, dp->d_name);
+
+            if (deleteFile || !isRegularFile(path)) {
+                command("delete", path);
+            }
+        }
+    }
+    rewinddir(dir);
+}
+
+void categorize(DIR *dir)
+{
+    char *basePath = ".";
+    char path[1000], data[307], filename[100], name[50], type[50], age[4];
+    struct dirent *dp;
+    
+    while ((dp = readdir(dir)) != NULL) {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
+            // Construct new path from the base path
+            sprintf(path, "%s/%s", basePath, dp->d_name);
+
+            if (!isRegularFile(path)) {
+                continue;
+            }
+            strcpy(filename, dp->d_name);
+
+            // Get every animal in a photo
+            char *save1, *save2;
+            char *animal = strtok_r(filename, "_", &save1);
+            while (animal) {
+                strcpy(type, strtok_r(animal, ";", &save2));
+                strcpy(name, strtok_r(NULL, ";", &save2));
+                strcpy(age, getAge(save2));
+
+                // Make sure that directory exist
+                DIR *target = getDir(type);
+                closedir(target);
+
+                // Write keterangan.txt
+                FILE *log = getLog(type); // getFile(dirPath)
+                fprintf(log, "nama : %s\n", name);
+                fprintf(log, "umur : %s tahun\n", age);
+                fputs("\n", log);
+                fclose(log);
+
+                // Copy file <filename> to file <name>
+                strcat(name, ".jpg");
+                sprintf(data, "%s|%s", dp->d_name, name);
+                command("copy", data);
+
+                // Move file to its appropriate folder
+                sprintf(data, "%s|%s", name, type);
+                command("move", data);
+
+                animal = strtok_r(NULL, "_", &save1);
+            }
+            command("delete", dp->d_name);
+        }
+    }
+    rewinddir(dir);
+}
+
+FILE *getLog(char *dirPath)
+{
+    char tmp[50];
+    sprintf(tmp, "%s/keterangan.txt", dirPath);
+    FILE *fp = fopen(tmp, "a+");
+    return fp;
+}
+
+char *getAge(char *input)
+{
+    int i = strlen(input) - 1;
+    if (!isdigit(input[i])) {
+        while (--i >= 0 && !isdigit(input[i]));
+        input[i + 1] = '\0';
+    }
+    return input;
+}
+
+bool isRegularFile(const char *path)
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return (S_ISREG(path_stat.st_mode) != 0);
+}
+
+DIR *getDir(char *basePath)
+{
+    DIR *dir = opendir(basePath);
+    if (!dir) {
+        command("make_dir", basePath);
+        dir = opendir(basePath);
+        deleteFolder(basePath, dir, true); // Clear initial folder
+    }
+    return dir;
+}
+
+void command(const char *command, char *path)
+{
+    int status;
+    pid_t child_id;
+    child_id = fork();
+
+    if (child_id < 0) {
+        printf("Fork failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    if (child_id == 0) {
+        // this is child
+        if (strcmp(command, "download") == 0) {
+            char *link = "https://docs.google.com/uc?export=download&id=1g5rehatLEkqvuuK_eooHJXB57EfdnxVD";
+            char *argv[] = {"wget", "--no-check-certificate", link, "-O", path, NULL};
+            execv("/bin/wget", argv);
+        }
+        else if (strcmp(command, "extract") == 0) {
+            char *argv[] = {"unzip", "-o", path, NULL};
+            execv("/bin/unzip", argv);
+        }
+        else if (strcmp(command, "delete") == 0) {
+            char *argv[] = {"rm", "-rf", path, NULL};
+            execv("/bin/rm", argv);
+        }
+        else if (strcmp(command, "make_dir") == 0) {
+            char src[50];
+            sprintf(src, "/home/%s/log", user);
+            char *argv[] = {"cp", "-r", src, path, NULL};
+            execv("/bin/cp", argv);
+        }
+        else if (strcmp(command, "move") == 0 || strcmp(command, "copy") == 0 ) {
+            // Parse data in path
+            char arr[2][50];
+            strcpy(arr[0], strtok(path, "|"));
+            strcpy(arr[1], strtok(NULL, "|"));
+
+            if (strcmp(command, "move") == 0) {
+                char *argv[] = {"mv", arr[0], arr[1], NULL};
+                execv("/bin/mv", argv);
+            }
+            else {
+                char *argv[] = {"cp", arr[0], arr[1], NULL};
+                execv("/bin/cp", argv);
+            }
+        }
+        else {
+            printf("Unrecognized command\n");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // this is parent
+        while (wait(&status) > 0);
+        return;
+    }
+}
